8db359e10c4b2c7d02ff80cb6fb65181cff59522
Manjunath Pujar <manjunathdtc3@gmail.com>
2025-01-21 13:25:05 +0530

    Reverse an Array

---
+++ src/arrays/ReverseAnArray_3.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ReverseAnArray_3 {
+    public static void reverseArray(int arr[]) {
+        // code here
+        int start=0;
+        int end = arr.length-1;
+        while(end>start) {
+            int temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+
+        System.out.println("Output: " + Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 20, 5, 6, 35};
+        System.out.println("Input: "+Arrays.toString(arr));
+        reverseArray(arr);
+    }
+}
