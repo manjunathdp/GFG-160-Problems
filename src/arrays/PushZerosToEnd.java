963e91038cf8380603041235d9d00a4bfcc383d1
Manjunath Pujar <manjunathdtc3@gmail.com>
2025-01-21 13:19:03 +0530

    Move All Zeroes to End

---
+++ src/arrays/PushZerosToEnd.java
@@ -0,0 +1,27 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class PushZerosToEnd {
+    public static void pushZerosToEnd(int[] arr) {
+        // code here
+        int nonZero = 0;
+        for(int i = 0; i<arr.length;i++) {
+            if(arr[i]!=0) {
+                arr[nonZero]=arr[i];
+                nonZero++;
+            }
+        }
+        for(int i = nonZero;i<arr.length;i++) {
+            arr[i]=0;
+        }
+        System.out.println("Output: "+Arrays.toString(arr));
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 20, 5, 6, 35};
+        System.out.println("Input: "+Arrays.toString(arr));
+        pushZerosToEnd(arr);
+    }
+}
